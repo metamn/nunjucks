c04d9461d703437f49bbead0b14f13bb666a82cb
metamn <bartus.csongor@gmail.com>
2016-02-24 18:47:13 +0200

    Include works

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp'),
     nunjucks = require('gulp-nunjucks'),
-    data = require('gulp-data');
+    data = require('gulp-data'),
+    rename = require('gulp-rename');
 
 gulp.task('default', function() {
   gulp.src('src/**/*.html.swig')
     .pipe(data(() => ({name: 'cccccs'})))
     .pipe(nunjucks.compile())
+    .pipe(rename({ extname: '' }))
     .pipe(gulp.dest('dist'))
 });
